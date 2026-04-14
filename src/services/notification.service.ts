15861219f768f6348720650edff8fe9eeb84d015
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2026-04-14 21:24:16 +0530

    fix: disable alert notification display in notification service handler

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -28,7 +28,6 @@ export const NotificationService = {
   init() {
     Notifications.setNotificationHandler({
       handleNotification: async () => ({
-        shouldShowAlert: true,
         shouldPlaySound: true,
         shouldSetBadge: true,
         shouldShowBanner: true,
